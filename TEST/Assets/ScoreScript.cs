73bdd87af07bee60777d99e178e3fa1294f8e7e9
DESKTOP-TNBOKMR\Acer <marwaha.raghav@rediffmail.com>
2019-03-27 01:54:04 +0530

    score system added

---
+++ TEST/Assets/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour {
+   public  Text Score;
+    public Text Ammu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float Scores= PlayerPrefs.GetFloat("Score");
+        Score.text = "Score: " + Scores;
+        int Ammo = PlayerPrefs.GetInt("AMMO");
+        Ammu.text = "AMMUNITION: " + Ammo;
+		
+	}
+}
--- TEST/Assets/Scripts/Game.cs
+++ TEST/Assets/Scripts/Game.cs
@@ -9,9 +9,13 @@ public class Game : MonoBehaviour
     private float pos_y;
     private float pos_z=-1;
     public float velocity = 0.01f;
+    private float DamagePoints=10f;
     public GameObject obj;
     public ParticleSystem leftbullet;
     public ParticleSystem rightbullet;
+    public float Hits=0;
+    public string Score;
+    public int Ammo = 10;
 
 
 
@@ -42,30 +46,36 @@ public class Game : MonoBehaviour
     public void Shooting()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Ammo>0 )
         {
             leftbullet.Play();
             rightbullet.Play();
+            
             if (hit)
             {
                 float HPoints = obj.GetComponent<ObstacleBehaviour>().ObstacleHealth();
                 Debug.Log("health" + HPoints);
-                HPoints = HPoints - 10f;
+                HPoints = HPoints - DamagePoints;
                 Debug.Log("hit" + hit.collider.name);
                 if (HPoints == 0)
                 {
                     Destroy(hit.collider.gameObject);
                 }
-
+                Hits++;
+                PlayerPrefs.SetFloat("Score", Hits);
+                
             }
-
+            Ammo--;
+            PlayerPrefs.SetInt("AMMO", Ammo);
         }
         else if (Input.GetKeyUp(KeyCode.Space))
-            {
+        {
             leftbullet.Stop();
             rightbullet.Stop();
         }
-    
+
+
     }
     private void Update()
     {
